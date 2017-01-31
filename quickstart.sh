be14674442acf979be801096bcd7ea526e70ede2
f-katkit <f-katkit@gmail.com>
2017-01-31 21:53:37 +0900

    fix sample script

--- quickstart.sh
+++ quickstart.sh
@@ -1,17 +1,12 @@
-# install phantomjs for unit test
-npm install -g --save-dev testem
-npm install -g --save-dev phantomjs-prebuilt
+## https://guides.emberjs.com/v2.11.0/getting-started/quick-start/
 
-# create ember project
-ember new winebar
-cd winebar
+# ember generate
+ember generate template application
+ember generate route scientists
+ember generate component people-list
 
-# install QUnit
-ember install ember-cli-qunit ember-cli-es5-shim
-
-# generate component
-ember generate component wine-stock
-
-ember test --server -m 'Integration | Component | wine stock'
-
-ember server
+# copy sample sources
+cp quickstart-sample/app/templates/application.hbs app/templates/application.hbs
+cp quickstart-sample/app/templates/scientists.hbs app/templates/scientists.hbs
+cp quickstart-sample/app/routes/scientists.js app/routes/scientists.js
+cp quickstart-sample/app/templates/components/people-list.hbs app/templates/components/people-list.hbs
