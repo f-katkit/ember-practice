e2993d56613b4582f6ad0d1740a764101e29bbe9
f-katkit <f-katkit@gmail.com>
2017-02-01 15:03:57 +0900

    fix shell script

--- unittest.sh
+++ unittest.sh
@@ -7,7 +7,7 @@ ember install ember-cli-qunit ember-cli-es5-shim
 ember g test-helper make-people-objects
 
 # copy source files
-cp -f unittest-sample/tests/helpers/make-people-objects.js /tests/helpers/make-people-objects.js
+cp -f unittest-sample/tests/helpers/make-people-objects.js tests/helpers/make-people-objects.js
 cp -f unittest-sample/tests/integration/components/people-list-test.js tests/integration/components/people-list-test.js
 
 # run test
